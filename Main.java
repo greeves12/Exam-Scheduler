5bd19fb34b607f25a9c10a5fda12695534c92eff
greeves12 <tategreeves@gmail.com>
2022-01-26 23:04:48 -0700

    Modularized data fetching

--- Main.java
+++ Main.java
@@ -3,15 +3,17 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/*
+* Known Bugs: Courses2 doesn't load course properly.
+* */
+
 public class Main {
+    private static boolean errorFlag = false;
+    private static String errorMessage = "";
+
     public static void main(String[] args) {
         boolean running = true;
-        boolean errorFlag = false;
-        String errorMessage = "";
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Room> rooms = new ArrayList<>();
-        ArrayList<Course> courses = new ArrayList<>();
-
 
         System.out.println("Exam Scheduler");
 
@@ -23,154 +25,176 @@ public class Main {
                 running = false;
             }
 
-            String[] arg = line.split(" ");
-            File courseFile = new File("test data/" + arg[0]);
-            File roomFile = new File("test data/" + arg[1]);
-            int slots = Integer.parseInt(arg[2]);
-            if(courseFile.exists()){
-                if(roomFile.exists()){
-                    if(slots > 0){
-                        try {
-                            Scanner sc = new Scanner(courseFile);
+            runDataFetch(line.split(" "));
 
-                            if(sc.hasNext()) {
-                                String data = sc.next();
-                                while (sc.hasNext()) {
-
-                                    String courseName;
-                                    ArrayList<Integer> students = new ArrayList<>();
-
-                                    if (Character.isLetter(data.charAt(0))) {
-                                        courseName = data;
-                                        for (int x = 0; x < courses.size(); x++) {
-                                            if (courses.get(x).getName().equals(data)) {
-                                                // System.out.println("ERROR: Duplicate course name found.");
-                                                errorFlag = true;
-                                                errorMessage = "ERROR: Duplicate course name found.";
-                                                break;
-                                            }
-                                        }
-                                        if(errorFlag)
-                                            break;
-
-                                        data = sc.next();
-                                        while (Character.isDigit(data.charAt(0))) {
-                                            if(!allNumbers(data)){
-                                                errorFlag = true;
-                                                errorMessage = "ERROR: StudentID contains non-digit";
-
-                                                break;
-                                            }
-
-                                            if (Integer.parseInt(data) < 0) {
-                                                //  System.out.println("ERROR: Negative studentID in " + courseName);
-                                                errorFlag = true;
-                                                errorMessage = "ERROR: Negative studentID in " + courseName;
-                                                break;
-                                            }
-
-                                            if(students.contains(Integer.valueOf(data))){
-                                                errorFlag = true;
-                                                errorMessage = "ERROR: Duplicate students in a class found.";
-                                                break;
-                                            }
-
-                                            students.add(Integer.valueOf(data));
-                                            if (!sc.hasNext()) {
-                                                break;
-                                            }
-                                            data = sc.next();
-                                        }
-                                        if (errorFlag) {
-                                            break;
-                                        }
-                                        courses.add(new Course(courseName, students));
-                                        System.out.println("PASS: Loaded Course " + courseName + " with size " + students.size());
-                                    } else {
-                                        //   System.out.println("ERROR: Non Letter Course");
-                                        errorFlag = true;
-                                        errorMessage = "ERROR: Non Letter Course";
-                                        break;
-                                    }
-                                }
-                            }else{
-                                errorFlag = true;
-                                errorMessage = "ERROR: Courses file is empty";
-                            }
-
-                            sc.close();
-
-
-                            if(!errorFlag){
-                                sc = new Scanner(roomFile);
-                                if(!sc.hasNext()){
-                                    errorFlag = true;
-                                    errorMessage = "ERROR: Rooms file is empty.";
-                                }else {
-                                    while (sc.hasNextLine()) {
-                                        String[] roomLine = sc.nextLine().split("\s+|\t+");
-
-                                        if(roomLine.length == 1){
-                                            errorFlag = true;
-                                            errorMessage = "ERROR: Invalid room.";
-                                            break;
-                                        }
-
-                                        if(!allNumbers(roomLine[1])){
-                                            errorFlag = true;
-                                            errorMessage = "ERROR: Room has non digits.";
-                                            break;
-                                        }else if(Integer.parseInt(roomLine[1]) < 0){
-                                            errorFlag = true;
-                                            errorMessage = "ERROR: Room is a negative number.";
-                                            break;
-                                        }
-
-                                        for(int x = 0; x < rooms.size(); x++){
-                                            if(rooms.get(0).getName().equals(roomLine[0])){
-                                                errorFlag = true;
-                                                errorMessage = "ERROR: Duplicate rooms found.";
-                                                break;
-                                            }
-                                        }
-
-                                        if(errorFlag){
-                                            break;
-                                        }
-
-                                        System.out.println("PASS: Loaded room " + roomLine[0] + " with capacity " + roomLine[1]);
-                                        rooms.add(new Room(roomLine[0],Integer.parseInt(roomLine[1])));
-                                    }
-
-                                }
-
-                            }
-
-                            if(errorFlag) {
-                                System.out.println(errorMessage);
-                                System.out.println("----- Exam Maker Cancelled Due To Error -----");
-                                errorFlag = false;
-                            }else{
-                                //execute exam formation
-                            }
-
-                        } catch (FileNotFoundException e) {
-                            e.printStackTrace();
-                        }
-                    }else{
-                        System.out.println("ERROR: Invalid slot #" + slots);
-                    }
-                }else{
-                    System.out.println("ERROR: Room file does not exist.");
-                }
-            }else{
-                System.out.println("ERROR: Course file does not exist.");
-            }
-
-            courses.clear();
-            rooms.clear();
         }
         scanner.close();
+    }
+
+    private static void runDataFetch(String[] arg){
+        ArrayList<Room> rooms = new ArrayList<>();
+        ArrayList<Course> courses = new ArrayList<>();
+
+        File courseFile = new File("test data/" + arg[0]);
+        File roomFile = new File("test data/" + arg[1]);
+        int slots = Integer.parseInt(arg[2]);
+        if(courseFile.exists()){
+            if(roomFile.exists()){
+                if(slots > 0){
+                    fetchCourses(courseFile);
+
+                    if(!errorFlag) {
+                        fetchRooms(roomFile);
+
+                        if(!errorFlag){
+                            //execute graph
+
+                            System.out.println("------ Time Slots ------");
+                        }
+                    }else{
+                        System.out.println(errorMessage);
+                    }
+
+                    errorFlag = false;
+                }else{
+                    System.out.println("ERROR: Invalid timeslot #" + slots);
+                }
+            }else{
+                System.out.println("ERROR: Room file does not exist.");
+            }
+        }else{
+            System.out.println("ERROR: Course file does not exist.");
+        }
+
+        courses.clear();
+        rooms.clear();
+    }
+
+    private static ArrayList<Course> fetchCourses(File courseFile){
+        ArrayList<Course> courses = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(courseFile);
+
+            if(sc.hasNext()) {
+                String data = sc.next();
+                while (sc.hasNext()) {
+
+                    String courseName;
+                    ArrayList<Integer> students = new ArrayList<>();
+
+                    if (Character.isLetter(data.charAt(0))) {
+                        courseName = data;
+                        for (int x = 0; x < courses.size(); x++) {
+                            if (courses.get(x).getName().equals(data)) {
+                                errorFlag = true;
+                                errorMessage = "ERROR: Duplicate course name found.";
+                                break;
+                            }
+                        }
+                        if(errorFlag)
+                            break;
+
+                        data = sc.next();
+                        while (Character.isDigit(data.charAt(0))) {
+                            if(!allNumbers(data)){
+                                errorFlag = true;
+                                errorMessage = "ERROR: StudentID contains non-digit";
+                                break;
+                            }
+
+                            if (Integer.parseInt(data) < 0) {
+                                errorFlag = true;
+                                errorMessage = "ERROR: Negative studentID in " + courseName;
+                                break;
+                            }
+
+                            if(students.contains(Integer.valueOf(data))){
+                                errorFlag = true;
+                                errorMessage = "ERROR: Duplicate students in a class found.";
+                                break;
+                            }
+
+                            students.add(Integer.valueOf(data));
+                            if (!sc.hasNext()) {
+                                break;
+                            }
+                            data = sc.next();
+                        }
+                        if (errorFlag) {
+                            break;
+                        }
+                        courses.add(new Course(courseName, students));
+                        System.out.println("PASS: Loaded Course " + courseName + " with size " + students.size());
+                    } else {
+                        errorFlag = true;
+                        errorMessage = "ERROR: Non Letter Course";
+                        break;
+                    }
+                }
+
+            }else{
+                errorFlag = true;
+                errorMessage = "ERROR: Courses file is empty";
+            }
+
+            sc.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return courses;
+    }
+
+    private static ArrayList<Room> fetchRooms(File roomFile){
+        ArrayList<Room> rooms = new ArrayList<>();
+
+        Scanner sc = null;
+        try {
+            sc = new Scanner(roomFile);
+
+            if(!sc.hasNext()){
+                errorFlag = true;
+                errorMessage = "ERROR: Rooms file is empty.";
+            }else {
+                while (sc.hasNextLine()) {
+                    String[] roomLine = sc.nextLine().split("\s+|\t+");
+
+                    if(roomLine.length == 1){
+                        errorFlag = true;
+                        errorMessage = "ERROR: Invalid room.";
+                        break;
+                    }
+
+                    if(!allNumbers(roomLine[1])){
+                        errorFlag = true;
+                        errorMessage = "ERROR: Room has non digits.";
+                        break;
+                    }else if(Integer.parseInt(roomLine[1]) < 0){
+                        errorFlag = true;
+                        errorMessage = "ERROR: Room is a negative number.";
+                        break;
+                    }
+
+                    for(int x = 0; x < rooms.size(); x++){
+                        if(rooms.get(0).getName().equals(roomLine[0])){
+                            errorFlag = true;
+                            errorMessage = "ERROR: Duplicate rooms found.";
+                            break;
+                        }
+                    }
+
+                    System.out.println("PASS: Loaded room " + roomLine[0] + " with capacity " + roomLine[1]);
+                    rooms.add(new Room(roomLine[0],Integer.parseInt(roomLine[1])));
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        return rooms;
     }
 
     private static boolean allNumbers(String s){
